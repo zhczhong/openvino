65a6c672106437d0748d924305f4c86a1ca208c4
Zhong, Zhicong <zhicong.zhong@intel.com>
2024-12-09 15:21:41 +0000

    support build without pre-built llvm

--- cmake/graph-compiler.cmake
+++ cmake/graph-compiler.cmake
@@ -26,6 +26,24 @@ if (NOT DEFINED GRAPH_COMPILER_LIBS)
         set(GC_ENABLE_BINDINGS_PYTHON OFF)
         set(OV_BUILD_SHARED_LIBS_TMP ${BUILD_SHARED_LIBS})
         set(BUILD_SHARED_LIBS OFF)
+
+        FetchContent_GetProperties(GC)
+        if(NOT GC_POPULATED)
+            FetchContent_Populate(GC)
+        endif()
+        if(NOT DEFINED LLVM_DIR OR NOT DEFINED MLIR_DIR)
+            execute_process(
+                COMMAND /bin/bash ./scripts/compile.sh --dev --llvm --imex
+                WORKING_DIRECTORY ${gc_SOURCE_DIR}
+            )
+
+            SET(LLVM_INST_PATH "${gc_SOURCE_DIR}/externals/llvm-project/build")
+            SET(LLVM_DIR "${LLVM_INST_PATH}/lib/cmake/llvm")
+            SET(MLIR_DIR "${LLVM_INST_PATH}/lib/cmake/mlir")
+            add_definitions(-DMLIR_RUNNER_UTILS_PATH=${LLVM_INST_PATH}/lib)
+            add_subdirectory(${gc_SOURCE_DIR} ${gc_BINARY_DIR} EXCLUDE_FROM_ALL)
+        endif()
+
         FetchContent_MakeAvailable(GC)
         set(BUILD_SHARED_LIBS ${OV_BUILD_SHARED_LIBS_TMP})
         FetchContent_GetProperties(GC BINARY_DIR gc_BINARY_DIR)
